60067753832be0f6fb0aa53e959820ef353388ca
hqz010 <6285767@qq.com>
2017-12-08 10:48:58 +0800

    备注修改

--- dubbo-demo-api/src/main/java/cn/javaxxw/service/UserService.java
+++ dubbo-demo-api/src/main/java/cn/javaxxw/service/UserService.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import cn.javaxxw.model.User;
 
+/**
+ * @author jason huang
+ * @version 1.0
+ * @desc
+ * @create 2017-11-29 14:31
+ **/
 public interface UserService {
 
     /**
